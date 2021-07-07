35df6ea3d9df23568e06a5701db14fb159300990
Divin Irakiza <irakizadivin@gmail.com>
2021-07-07 19:56:18 +0200

    (new): Downleveling

--- 05. downleveling/main.ts
+++ 05. downleveling/main.ts
@@ -8,3 +8,6 @@ function greet(person: string, date: Date) {
   // `Hello ${person}, today is ${date.toDateString()}!`;
   //     changes to
   // "Hello " + person + ", today is " + date.toDateString() + "!";
+
+
+  // but running with tsc --target es2015 gives the same output
