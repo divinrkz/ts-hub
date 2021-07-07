ce5551bcd3fb1b182017d76955ff89e78b676776
Divin Irakiza <irakizadivin@gmail.com>
2021-07-07 19:55:11 +0200

    (new): Downleveling

---
+++ 05. downleveling/main.js
@@ -0,0 +1,4 @@
+function greet(person, date) {
+    console.log("Hello " + person + ", today is " + date.toDateString() + "!");
+}
+greet("Maddison", new Date());
---
+++ 05. downleveling/main.ts
@@ -0,0 +1,10 @@
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+  }
+  
+  greet("Maddison", new Date());
+
+  // when compliling with tsc
+  // `Hello ${person}, today is ${date.toDateString()}!`;
+  //     changes to
+  // "Hello " + person + ", today is " + date.toDateString() + "!";
