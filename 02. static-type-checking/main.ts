c59b177c871271087061ec5c987f73f91a144640
Divin Irakiza <irakizadivin@gmail.com>
2021-07-07 19:37:02 +0200

    (new): Non-exception Failures and Legitimate bugs

--- 02. static-type-checking/main.ts
+++ 02. static-type-checking/main.ts
@@ -1,3 +1,4 @@
 const message = 'TS';
 
 message();
+// This expression is not callable. Type 'String' has no call signatures.
---
+++ 03. non-exception-failures/main.ts
@@ -0,0 +1,7 @@
+const user = {
+    name: 'user',
+    age: 26
+};
+
+user.location;
+// Property 'location' does not exist on type '{ name: string; age: number; }'.
