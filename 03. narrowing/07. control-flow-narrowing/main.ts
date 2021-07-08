95cf266d79ec889e6bd7ccc443e6e3bbe4187a4c
Divin Irakiza <irakizadivin@gmail.com>
2021-07-08 20:38:08 +0200

    (new): Control flow analysis narrowing

---
+++ 03. narrowing/07. control-flow-narrowing/main.ts
@@ -0,0 +1,26 @@
+
+function example() {
+    let x: string | number | boolean;
+  
+    x = Math.random() < 0.5;
+  
+    console.log(x);
+               
+  let x: boolean
+  
+    if (Math.random() < 0.5) {
+      x = "hello";
+      console.log(x);
+                 
+  let x: string
+    } else {
+      x = 100;
+      console.log(x);
+                 
+  let x: number
+    }
+  
+    return x;
+          
+  let x: string | number
+  }
