4c52fcb8f549a467f4706615993b1387f4f1d68f
Divin Irakiza <irakizadivin@gmail.com>
2021-07-12 21:07:36 +0200

    Added more on function overloads

--- 04. functions/10. function-overloads/main.ts
+++ 04. functions/10. function-overloads/main.ts
@@ -16,3 +16,38 @@ const d2 = makeDate(5, 5, 5);
 
 const d3 = makeDate(1, 3);
 // No overload expects 2 arguments, but overloads do exist that expect either 1 or 3 arguments.
+
+
+function fn(x: string): void;
+function fn() {
+  // ...
+}
+// Expected to be able to call with zero arguments
+fn();
+
+
+
+
+function fn(x: boolean): void;
+// Argument type isn't right
+function fn(x: string): void;
+// This overload signature is not compatible with its implementation signature.
+function fn(x: boolean) {}
+
+
+
+
+// Let’s consider a function that returns the length of a string or an array:
+
+function len(s: string): number;
+function len(arr: any[]): number;
+function len(x: any) {
+  return x.length;
+}
+
+
+
+// A better approach 
+function len(x: any[] | string) {
+  return x.length;
+}
