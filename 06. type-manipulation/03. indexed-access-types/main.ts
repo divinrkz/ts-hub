54cad6fd34b334fc7de02bdbf6e0b2ce13df3947
Divin Irakiza <irakizadivin@gmail.com>
2021-07-13 11:57:03 +0200

    added indexed access-types manipulation

---
+++ 06. type-manipulation/03. indexed-access-types/main.ts
@@ -0,0 +1,41 @@
+type Person = {age: number, name: string, alive: boolean};
+
+type Age = Person["age"];
+// type Age = number;
+
+
+type T1 = Person["age" | "name"];
+// type T1 = string | number;
+
+
+type T2 = Person[keyof Person];
+// type T2 = string | number | boolean
+
+
+type AliveOrName = "alive" | "name";
+type T3 = Person[AliveOrName];
+// type td3 = string | boolean
+
+
+
+
+// Advanced example
+
+const MyArray = [
+  { name: "Alice", age: 15 },
+  { name: "Bob", age: 23 },
+  { name: "Eve", age: 38 },
+];
+
+type Person = typeof MyArray[number];   
+// type Person = {
+//     name: string;
+//     age: number;
+// }
+
+type Age = typeof MyArray[number]["age"];
+// type Age = number
+
+// Or
+type Age2 = Person["age"];
+// type Age2 = number
