6fa467463f258dbe40fd6608e1d4935f5812ead3
Divin Irakiza <irakizadivin@gmail.com>
2021-07-13 11:35:45 +0200

    addded typeof-operator limitations

--- 06. type-manipulation/02. typeof-operator/main.ts
+++ 06. type-manipulation/02. typeof-operator/main.ts
@@ -18,3 +18,15 @@ type P = ReturnType<f>;
 // 'f' refers to a value, but is being used as a type here. Did you mean 'typeof f'?
 
 type P = ReturnType<typeof f>;
+
+// type P = {
+//     x: number;
+//     y: number;
+// }
+
+
+
+// LIMITATIONS
+
+// Meant to use = ReturnType<typeof msgbox>
+let shouldContinue: typeof msgbox("Are you sure you want to continue?");
