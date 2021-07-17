f9f347ea36eb1ed3f4fec2b2cbc874f6a5096fa9
DivinIrakiza <irakizadivin@gmail.com>
2021-07-17 19:50:31 +0200

    (new): Mapping modifiers

--- 06. type-manipulation/07. mapping-modifiers/main.ts
+++ 06. type-manipulation/07. mapping-modifiers/main.ts
@@ -9,3 +9,23 @@ type CreateMutable<Type> = {
   };
   
   type UnlockedAccount = CreateMutable<LockedAccount>;
+
+
+
+
+
+
+
+
+  // Removes 'optional' attributes from a type's properties
+type Concrete<Type> = {
+    [Property in keyof Type]-?: Type[Property];
+  };
+  
+  type MaybeUser = {
+    id: string;
+    name?: string;
+    age?: number;
+  };
+  
+  type User = Concrete<MaybeUser>;
