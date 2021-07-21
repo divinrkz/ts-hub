3fb934e4bda0e87fc44f7379a6d09301cb8f9499
DivinIrakiza <irakizadivin@gmail.com>
2021-07-21 23:28:00 +0200

    (new): Pick<T> explained

---
+++ 09. utility-types/05. pick-type/main.ts
@@ -0,0 +1,15 @@
+// Pick<Type, Keys>
+// Constructs a type by picking the set of properties Keys (string literal or union of string literals) from Type.
+
+interface Todo {
+    title: string;
+    description: string;
+    completed: boolean;
+}
+
+type TodoPreview = Pick<Todo, 'title' | 'completed'>;
+
+const todo:TodoPreview = {
+    title: 'Clean room',
+    completed: false
+}
